0ba4a6235d48ff62229c01b28987d594c90d6667
Ming Duan <11203duan@gmail.com>
2019-03-20 01:12:10 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,33 @@
+class Artist
+
+attr_attributor :artist_name
+
+end
+
+class Author
+
+attr_attributor :author_name
+
+
+end
+
+class Post
+
+attr_attributor :post_name
+
+end
+
+class Song
+
+attr_attributor :song_name
+
+end
+
+
+
+
+
+
+
+
+  
